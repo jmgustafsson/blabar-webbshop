132c13f703b3ecc747e660a1d3760c969740d407
jenseneducation-john-gustafsson <john.gustafsson80@gmail.com>
2021-03-11 16:25:01 +0100

    lagt till grid i about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,37 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm">
+        Produkt 1
+      </div>
+      <div class="col-sm">
+        Produkt 2
+      </div>
+      <div class="col-sm">
+        Produkt 3
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm">
+        Produkt 4
+      </div>
+      <div class="col-sm">
+        Produkt 5
+      </div>
+      <div class="col-sm">
+        Produkt 6
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm">
+        Produkt 7
+      </div>
+      <div class="col-sm">
+        Produkt 8
+      </div>
+      <div class="col-sm">
+        Produkt 9
+      </div>
+    </div>
   </div>
 </template>
