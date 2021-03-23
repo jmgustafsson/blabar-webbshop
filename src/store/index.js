3f9c2f5540fb24a8e43cce16a269e6535aae7127
jenseneducation-john-gustafsson <john.gustafsson80@gmail.com>
2021-03-23 16:07:25 +0100

    lagt till all funktionalitet i vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,111 @@
+/* eslint-disable */
 import Vue from 'vue'
 import Vuex from 'vuex'
+import shop from '../api/shop'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {
+    state: { // = data
+        products: [],
+        // {id, quantity}
+        cart: [],
+        checkoutStatus: null
     },
+
+    getters: { // = computed properties
+        availableProducts(state, getters) {
+            return state.products.filter(product => product)
+        },
+
+        cartProducts(state) {
+            return state.cart.map(cartItem => {
+                const product = state.products.find(product => product.id === cartItem.id)
+                return {
+                    name: product.name,
+                    price: product.price,
+                    quantity: cartItem.quantity
+                }
+            })
+        },
+
+        cartTotal(state, getters) {
+            return getters.cartProducts.reduce((total, product) => total + product.price * product.quantity, 0)
+        },
+
+        cartQuantity(state, getters) {
+            return getters.cartProducts.reduce((total, product) => total + product.quantity, 0);
+        }
+    },
+
+    actions: { // = methods
+        fetchProducts({ commit }) {
+            return new Promise((resolve, reject) => {
+                // make the call
+                // run setProducts mutations
+                shop.getProducts((products) => {
+                    commit("setProducts", products);
+                    resolve()
+                });
+            })
+        },
+
+        addProductToCart(context, product) {
+            if (product) {
+                // find cartItem
+                const cartItem = context.state.cart.find(item => item.id === product.id)
+                if (!cartItem) {
+                    // pushProductToCart
+                    context.commit("pushProductToCart", product.id)
+                } else {
+                    // incrementItemQuantity
+                    context.commit("incrementItemQuantity", cartItem)
+                }
+                context.commit("decrementProductInventory", product)
+            }
+        },
+
+        checkout({ state, commit }) {
+            shop.buyProducts(
+                state.cart,
+                () => {
+                    commit("emptyCart")
+                    commit("setCheckoutStatus", "success")
+                },
+                () => {
+                    commit("setCheckoutStatus", "fail")
+                }
+            )
+        }
+    },
+
     mutations: {
+        setProducts(state, products) {
+            // update products
+            state.products = products
+        },
+
+        pushProductToCart(state, productId) {
+            state.cart.push({
+                id: productId,
+                quantity: 1
+            })
+        },
+
+        incrementItemQuantity(state, cartItem) {
+            cartItem.quantity++
+        },
+
+        decrementProductInventory(state, product) {
+            product.inventory--
+        },
+
+        setCheckoutStatus(state, status) {
+            state.checkoutStatus = status
+        },
+
+        emptyCart(state) {
+            state.cart = []
+        }
     },
-    actions: {
-    },
-    modules: {
-    }
 })
