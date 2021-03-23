c7ee5edec6ea5ef3c67bb48b454600ca9ea8860c
jenseneducation-john-gustafsson <john.gustafsson80@gmail.com>
2021-03-23 16:13:42 +0100

    lagt till innehåll i checkout.vue

---
+++ src/views/Checkout.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="center-screen">
+    <h1 class="header">Kassan</h1>
+    <section>
+      <ul>
+        <li v-for="product in products" :key="product.id">
+          {{ product.name }} {{ product.price }}:- {{ product.quantity }}st
+        </li>
+      </ul>
+      <p class="totalsum">Summa: {{ total }}:-</p>
+      <button @click="$store.dispatch('checkout')" class="order-button">
+        Beställ
+      </button>
+      <p v-if="$store.state.checkoutStatus">
+        {{ $store.state.checkoutStatus }}
+      </p>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    products() {
+      return this.$store.getters.cartProducts;
+    },
+
+    total() {
+      return this.$store.getters.cartTotal;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+* {
+  box-sizing: border-box;
+  font-weight: normal;
+  margin: 0;
+  padding: 0;
+}
+
+.center-screen {
+  text-align: center;
+}
+
+.header {
+  margin-top: 20px;
+  text-align: center;
+  font-size: 50px;
+}
+
+.totalsum {
+  text-align: center;
+  margin: 20px 0;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+
+  li {
+    margin: 5px 0;
+  }
+}
+
+.order-button {
+  background: rgb(10, 113, 148);
+  border: 0;
+  border-radius: 10px;
+  outline: none;
+  color: white;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: bold;
+  height: 2.5rem;
+  width: 10rem;
+}
+</style>
