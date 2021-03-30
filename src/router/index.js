759c2c7a5aef8302f71932c44c63cf98119f2235
Romsep <chilescote1@hotmail.com>
2021-03-30 16:34:55 +0200

    la till router path

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Checkout from "../views/Checkout.vue";
+import SignUp from '../views/SignUp.vue';
+import Login from "../views/Login.vue";
+import Signed from '../views/Signed.vue';
 
 Vue.use(VueRouter);
 
@@ -16,6 +19,21 @@ const routes = [
     name: "Checkout",
     component: Checkout
   },
+  {
+    path: "/signup",
+    name: "signup",
+    component: SignUp
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login
+  },
+  {
+    path: "/signed",
+    name: "signed",
+    component: Signed
+  }
 ];
 
 const router = new VueRouter({
