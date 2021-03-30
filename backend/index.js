0504fcf18c4936f2e6e41a7e9bfbabe559f0535b
Romsep <chilescote1@hotmail.com>
2021-03-30 16:31:07 +0200

    uppdaterad index.js

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,18 @@ import express from "express";
 import cors from "cors";
 import Router from "./routes/routes.js";
 
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(cors());
+
+
 app.use(Router);
+
+const router = require('./routes/routes.js');
+app.use('/api', router);
+
 
 app.listen(5000, () => console.log("Server running on port 5000"));
