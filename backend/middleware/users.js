4e72c6aefdc0ddfd121777c936fe6575006f4c83
Romsep <chilescote1@hotmail.com>
2021-04-01 09:29:39 +0200

    svenska översättning och err msg

--- backend/middleware/users.js
+++ backend/middleware/users.js
@@ -6,14 +6,14 @@ export default {
     // username min length 3
     if (!req.body.username || req.body.username.length < 3) {
       return res.status(400).send({
-        msg: "Snälla ange ett användarnamn med minst 3 tecken",
+        msg: "Snälla ange ett användarnamn med minst 3 tecken"
       });
     }
 
     // password min 6 chars
     if (!req.body.password || req.body.password.length < 6) {
       return res.status(400).send({
-        msg: "Snälla ange ett lösenord med minst 6 tecken",
+        msg: "Snälla ange ett lösenord med minst 6 tecken"
       });
     }
 
@@ -23,7 +23,7 @@ export default {
       req.body.password != req.body.password_repeat
     ) {
       return res.status(400).send({
-        msg: "Både lösernord måste matcha",
+        msg: "Både lösernord måste matcha"
       });
     }
 
@@ -38,8 +38,8 @@ export default {
       next();
     } catch (err) {
       return res.status(401).send({
-        msg: "Din session är invalid!",
+        msg: "Din session är invalid!"
       });
     }
-  },
+  }
 };
